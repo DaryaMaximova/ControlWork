20e1311f613a526eed05fc4e21c717b175a55379
DaryaMaximova <darya_m@74.ru>
2022-09-19 18:33:33 +0500

    add method NewArr

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+//Примеры:
+//[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+
+string[] arr1 = new string[3] {"Rus", ":-)", "Kaz"};
+string[] arr2 = new string[arr1.Length];
+void NewArr(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3) arr2[count] = arr1[i]; count++;    
+    }
+}
+
